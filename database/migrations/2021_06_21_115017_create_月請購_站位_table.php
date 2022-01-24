ad0eadcf86818b54e61fb20d36793998fd9ee1d7
MacBook Pega <Vincent6_Yeh@pegatroncorp.com>
2022-01-24 15:46:28 +0800

    0124 fixed migration and seeder on 月請購_單耗 and 月請購_站位

--- database/migrations/2021_06_21_115017_create_月請購_站位_table.php
+++ database/migrations/2021_06_21_115017_create_月請購_站位_table.php
@@ -34,7 +34,7 @@ class Create月請購站位Table extends Migration
             $table->string('畫押信箱', 45);
             $table->dateTime('畫押時間')->nullable();
             $table->string('紀錄',255)->nullable();
-            $table->string('送單時間', 45);
+            $table->dateTime('送單時間');
             $table->string('送單人', 45);
             //$table->timestamps();
             //$table->softDeletes();
