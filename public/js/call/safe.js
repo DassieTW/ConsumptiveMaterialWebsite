2fa5bf7ee2b4deac63d053750c7a19550ae31911
t105590005 <t22923200@gmail.com>
2022-04-07 15:08:23 +0800

    0407

--- public/js/call/safe.js
+++ public/js/call/safe.js
@@ -54,96 +54,92 @@ $(document).ready(function () {
 
 
     var count = $("#count").val();
-    for(var i = 0 ; i < count ; i++){
-        var safe = $("#safea"+i).html();
-        var stock = $("#stocka"+i).html();
-        var percent = stock / safe ;
+    for (var i = 0; i < count; i++) {
+        var safe = $("#safea" + i).html();
+        var stock = $("#stocka" + i).html();
+        var percent = stock / safe;
         console.log(percent);
 
-        if(percent < 0.5)
-        {
-            $("#safea"+i).css("background-color", "red");
-        }
-        else if(percent >= 0.5 && percent < 0.8)
-        {
-            $("#safea"+i).css("background-color", "orange");
-        }
-        else if(percent >= 0.8)
-        {
-            $("#safea"+i).css("background-color", "yellow");
-        }
-        else
-        {
-            $("#safea"+i).css("background-color", "red");
+        if (percent < 0.5) {
+            $("#safea" + i).css("background-color", "red");
+        } else if (percent >= 0.5 && percent < 0.8) {
+            $("#safea" + i).css("background-color", "orange");
+        } else if (percent >= 0.8) {
+            $("#safea" + i).css("background-color", "yellow");
+        } else {
+            $("#safea" + i).css("background-color", "red");
         }
 
     }
 
     var count1 = $("#count1").val();
-    for(var i = 0 ; i < count1 ; i++){
-        var safe = $("#safeb"+i).html();
-        var stock = $("#stockb"+i).html();
-        var percent = stock / safe ;
+    for (var i = 0; i < count1; i++) {
+        var safe = $("#safeb" + i).html();
+        var stock = $("#stockb" + i).html();
+        var percent = stock / safe;
         console.log(percent);
 
-        if(percent < 0.5)
-        {
-            $("#safeb"+i).css("background-color", "red");
-        }
-        else if(percent >= 0.5 && percent < 0.8)
-        {
-            $("#safeb"+i).css("background-color", "orange");
-        }
-        else if(percent >= 0.8)
-        {
-            $("#safeb"+i).css("background-color", "yellow");
-        }
-        else
-        {
-            $("#safeb"+i).css("background-color", "red");
+        if (percent < 0.5) {
+            $("#safeb" + i).css("background-color", "red");
+        } else if (percent >= 0.5 && percent < 0.8) {
+            $("#safeb" + i).css("background-color", "orange");
+        } else if (percent >= 0.8) {
+            $("#safeb" + i).css("background-color", "yellow");
+        } else {
+            $("#safeb" + i).css("background-color", "red");
         }
 
     }
 
     var count2 = $("#count2").val();
-    for(var i = 0 ; i < count2 ; i++){
-        var safe = $("#safec"+i).html();
-        var stock = $("#stockc"+i).html();
-        var percent = stock / safe ;
+    for (var i = 0; i < count2; i++) {
+        var safe = $("#safec" + i).html();
+        var stock = $("#stockc" + i).html();
+        var percent = stock / safe;
         console.log(percent);
-        if(percent < 0.5)
-        {
-            $("#safec"+i).css("background-color", "red");
-        }
-        else if(percent >= 0.5 && percent < 0.8)
-        {
-            $("#safec"+i).css("background-color", "orange");
-        }
-        else if(percent >= 0.8)
-        {
-            $("#safec"+i).css("background-color", "yellow");
-        }
-        else
-        {
-            $("#safec"+i).css("background-color", "red");
+        if (percent < 0.5) {
+            $("#safec" + i).css("background-color", "red");
+        } else if (percent >= 0.5 && percent < 0.8) {
+            $("#safec" + i).css("background-color", "orange");
+        } else if (percent >= 0.8) {
+            $("#safec" + i).css("background-color", "yellow");
+        } else {
+            $("#safec" + i).css("background-color", "red");
         }
 
     }
 
-    var number = [];
-    var client = [];
-    var remark = [];
+    $('#safe').on('submit', function (e) {
+        e.preventDefault();
 
-    for (let i = 0; i < count; i++) {
-        if($("#remark" + i).val() !== ""){
-            client.push($("#client" + i).val());
-            number.push($("#number" + i).val());
-            remark.push($("#remark" + i).val());
+        var number = [];
+        var client = [];
+        var remark = [];
+
+        for (let i = 0; i < count; i++) {
+            if ($("#remark" + i).val() !== "") {
+                client.push($("#client" + i).val());
+                number.push($("#number" + i).val());
+                remark.push($("#remark" + i).val());
+            }
         }
-    }
+        for (let i = 0; i < count1; i++) {
+            if ($("#remarka" + i).val() !== "") {
+                client.push($("#clienta" + i).val());
+                number.push($("#numbera" + i).val());
+                remark.push($("#remarka" + i).val());
+            }
+        }
+        for (let i = 0; i < count2; i++) {
+            if ($("#remarkb" + i).val() !== "") {
+                client.push("非月請購");
+                number.push($("#numberb" + i).val());
+                remark.push($("#remarkb" + i).val());
+            }
+        }
 
 
-
+    });
 
 
 
