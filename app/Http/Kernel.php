974f622241f95889d3abf79c032eef3ff522fa64
Pegatron <pega@Pegatrons-MacBook-Pro>
2023-05-29 17:33:58 +0800

    Update to allow CORS from MIS site.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'priority' => \App\Http\Middleware\Priority::class,
         'locale' => \App\Http\Middleware\SetLocale::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
--- routes/web/web.php
+++ routes/web/web.php
@@ -51,7 +51,7 @@ Route::get('/', function () {
     else {
         return view('welcome');
     } // else
-})->name('welcome')->withoutMiddleware('auth');
+})->name('welcome')->middleware('cors')->withoutMiddleware('auth');
 
 Route::get('/hello_world', function () {
     return view('hello_world');
