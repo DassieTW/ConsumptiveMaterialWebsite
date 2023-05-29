6af22574d9407d6300351c5edb22e481af8dc9fd
Pegatron <pega@Pegatrons-MacBook-Pro>
2023-05-29 18:32:52 +0800

    VerifyCsrfToken added exception URIs.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,7 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         'https://ws.ecomp.pegatroncorp.com/*',
-        'http://ws.ecomp.pegatroncorp.com/*'
+        'http://ws.ecomp.pegatroncorp.com/*',
+        'https://ws.ecomp.pegatroncorp.com'
     ];
 }
