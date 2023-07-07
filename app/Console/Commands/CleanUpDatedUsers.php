9edd5e138052e8cba3312038416482a0de297ee1
Pegatron <pega@Pegatrons-MacBook-Pro>
2023-07-07 13:59:08 +0800

    Update CleanUpDatedUsers artisan command to exclude su account

--- app/Console/Commands/CleanUpDatedUsers.php
+++ app/Console/Commands/CleanUpDatedUsers.php
@@ -47,6 +47,7 @@ class CleanUpDatedUsers extends Command
                 \Config::set('database.connections.' . env("DB_CONNECTION") . '.database', $site);
                 \DB::purge(env("DB_CONNECTION"));
                 \DB::table('login')
+                    ->where('username', '!=', 'su')
                     ->whereDate('last_login_time', '<=', \Carbon\Carbon::now()->modify('-6 months')->toDateTimeString())
                     ->orWhereNull('last_login_time')
                     ->delete();
