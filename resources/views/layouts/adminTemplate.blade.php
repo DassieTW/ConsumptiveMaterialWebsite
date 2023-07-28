64d49d31724173eecd444bee1a24ed5382d964d6
Pegatron <pega@Pegatrons-MacBook-Pro>
2023-07-28 18:48:42 +0800

    Added switch site LoginPolicy and updated adminTemplate. Backend not yet implemeted.

--- resources/views/layouts/adminTemplate.blade.php
+++ resources/views/layouts/adminTemplate.blade.php
@@ -84,8 +84,7 @@
                                     <a class="sidebar-brand" href="{{ url('/') }}">
                                         <span class="sidebar-brand-text align-middle">
                                             {!! __('templateWords.websiteName') !!}
-                                            {{-- <sup><small class="badge bg-primary text-uppercase">Pro</small></sup>
-                                            --}}
+                                            {{-- <sup><small class="badge bg-primary text-uppercase">Pro</small></sup> --}}
                                         </span>
                                         <svg class="sidebar-brand-icon align-middle" width="32px" height="32px"
                                             viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" stroke-width="1.5"
@@ -96,7 +95,6 @@
                                             <path d="M20 16L12 20L4 16"></path>
                                         </svg>
                                     </a>
-
 
                                     <ul class="sidebar-nav">
                                         {{-- <li class="sidebar-header">
@@ -1138,21 +1136,55 @@
                             </a>
                             <div class="dropdown-menu dropdown-menu-end align-items-center"
                                 style="border-radius: 10px;">
-                                <a class="dropdown-item" href="#">
+                                <a class="dropdown-item" href="{{ url('/member/change') }}">
                                     <i class="p-0 mr-1" data-feather="user"></i>
                                     <span>Profile</span>
                                 </a>
-                                <a class="dropdown-item disabled align-items-center" style="color : #495057;"
-                                    href="#">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
-                                        fill="currentColor" class="bi bi-building p-0 mr-1" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd"
-                                            d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z" />
-                                        <path
-                                            d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z" />
-                                    </svg>
-                                    <span>{{ str_replace('Consumables management', '', \Session::get('database')) }}</span>
-                                </a>
+
+                                @can('canSwitchSites', App\Models\Login::class)
+                                    <a class="dropdown-item align-items-center" data-bs-toggle="collapse"
+                                        data-bs-target="#sitesMenu" aria-expanded="false">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                                            fill="currentColor" class="bi bi-building p-0 mr-1" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd"
+                                                d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z" />
+                                            <path
+                                                d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z" />
+                                        </svg>
+                                        <span>{{ str_replace('Consumables management', '', \Session::get('database')) }}</span>
+                                    </a>
+                                    <div class="collapse" id="sitesMenu">
+                                        @php
+                                            $database_list = config('database_list.databases');
+                                            $database_names = [];
+                                            foreach ($database_list as $value) {
+                                                $temp = str_replace(' Consumables management', '', $value);
+                                                array_push($database_names, $temp);
+                                            } // for each
+                                            
+                                            unset($value); // unset the var created in the foreach loop
+                                        @endphp
+                                        @for ($i = 1; $i < count($database_list); $i++)
+                                            @if (\Session::get('database') != $database_list[$i])
+                                                <a class="dropdown-item justify-content-center" href="#"
+                                                    value="{{ $database_list[$i] }}">
+                                                    {{ $database_names[$i] }}</a>
+                                            @endif
+                                        @endfor
+                                    </div>
+                                @else
+                                    <a class="dropdown-item disabled align-items-center" style="color : #495057;"
+                                        href="#">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                                            fill="currentColor" class="bi bi-building p-0 mr-1" viewBox="0 0 16 16">
+                                            <path fill-rule="evenodd"
+                                                d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694 1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z" />
+                                            <path
+                                                d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z" />
+                                        </svg>
+                                        <span>{{ str_replace('Consumables management', '', \Session::get('database')) }}</span>
+                                    </a>
+                                @endcan
                                 <a class="dropdown-item" data-bs-toggle="collapse" data-bs-target="#langMenu"
                                     aria-expanded="false">
                                     <i class="p-0 mr-1" data-feather="book-open"></i>
