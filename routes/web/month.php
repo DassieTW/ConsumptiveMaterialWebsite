0afd980229b3945e594e53a10489e1b22c91a643
Pegatron <pega@Pegatrons-MacBook-Pro>
2023-09-05 15:38:45 +0800

    test

--- routes/web/month.php
+++ routes/web/month.php
@@ -266,9 +266,10 @@ Route::post('/uploadmonth', [MonthController::class, 'uploadmonth'])->name('mont
 //單耗畫押page
 Route::get('/testconsume', function () {
     if (
-        str_contains(url()->previous(), "testconsume") && request()->query('SSOfailed', 'false') == 'false'
+        str_contains(url()->previous(), "testconsume") &&
+        request()->query('SSOfailed', 'false') == 'false'
     ) {
-        // do nothing
+        return back();
     } else {
         if (strcmp(env('APP_ENV'), 'production') === 0 && request()->query('SSOfailed', 'false') == 'false') {
             // redirect to MIS SSO page
