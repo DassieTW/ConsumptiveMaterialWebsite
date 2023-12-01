8c5b0a7df7ef282f87e923cb5ec7b01ace9373d9
Pegatron <pega@Pegatrons-MacBook-Pro>
2023-12-01 20:02:12 +0800

    Alter pr_review layout

--- resources/views/mail/pr_review.blade.php
+++ resources/views/mail/pr_review.blade.php
@@ -5,58 +5,73 @@
     <meta charset="utf-8">
     <title>{{ __('monthlyPRpageLang.page_name') }}</title>
     <style>
-        table, th, td {
-            border: 3px solid black;
+        table th,
+        table td {
+            padding: .4em;
+            text-align: center;
+        }
+
+        table,
+        tr,
+        td {
+            border: 2px solid black;
             border-collapse: collapse;
         }
 
         table {
+            table-layout: fixed;
             width: 100%;
             height: 100%;
+        }
+
+        table td {
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
         }
     </style>
 </head>
 
 <body>
     <h2>{{ __('monthlyPRpageLang.page_name') }}</h1>
-    <div class="w-100" style="height: 1ch;"></div><!-- </div>breaks cols to a new line-->
-    <h2>Please have a look at the attached file.</h2>
-    <table>
-        <thead>
-            <tr>
-                <th>{{ __('monthlyPRpageLang.isn') }}</th>
-                <th>{{ __('monthlyPRpageLang.pName') }}</th>
-                <th>{{ __('monthlyPRpageLang.format') }}</th>
-                <th>{{ __('monthlyPRpageLang.price') }}</th>
-                <th>{{ __('monthlyPRpageLang.nowneed') }}</th>
-                <th>{{ __('monthlyPRpageLang.nextneed') }}</th>
-                <th>{{ __('monthlyPRpageLang.nowstock') }}</th>
-                <th>{{ __('monthlyPRpageLang.transit') }}</th>
-                <th>{{ __('monthlyPRpageLang.buyamount') }}</th>
-                <th>{{ __('monthlyPRpageLang.buyprice') . '(' . $Currency1 . ')' }}</th>
-                <th>{{ __('monthlyPRpageLang.buyprice') . '(' . $Currency2 . ' ' . $Rate . ')' }}</th>
-                <th>{{ __('monthlyPRpageLang.moq') }}</th>
-            </tr>
-        </thead>
-        <tbody>
-            @for ($a = 0; $a < count($PN); $a++)
+        <div class="w-100" style="height: 1ch;"></div><!-- </div>breaks cols to a new line-->
+        <h2>Please have a look at the attached file.</h2>
+        <table>
+            <thead>
                 <tr>
-                    <td>{{ $PN[$a] }}</td>
-                    <td>{{ $pName[$a] }}</td>
-                    <td>{{ $Spec[$a] }}</td>
-                    <td>{{ $Unit_price[$a] }}</td>
-                    <td>{{ $nowNeed[$a] }}</td>
-                    <td>{{ $nextNeed[$a] }}</td>
-                    <td>{{ $Stock[$a] }}</td>
-                    <td>{{ $in_Transit[$a] }}</td>
-                    <td>{{ $ReqAmount[$a] }}</td>
-                    <td>{{ $total_price1[$a] }}</td>
-                    <td>{{ $total_price2[$a] }}</td>
-                    <td>{{ $MOQ[$a] }}</td>
+                    <th>{{ __('monthlyPRpageLang.isn') }}</th>
+                    <th>{{ __('monthlyPRpageLang.pName') }}</th>
+                    <th>{{ __('monthlyPRpageLang.format') }}</th>
+                    <th>{{ __('monthlyPRpageLang.price') }}</th>
+                    <th>{{ __('monthlyPRpageLang.nowneed') }}</th>
+                    <th>{{ __('monthlyPRpageLang.nextneed') }}</th>
+                    <th>{{ __('monthlyPRpageLang.nowstock') }}</th>
+                    <th>{{ __('monthlyPRpageLang.transit') }}</th>
+                    <th>{{ __('monthlyPRpageLang.buyamount') }}</th>
+                    <th>{{ __('monthlyPRpageLang.buyprice') . '(' . $Currency1 . ')' }}</th>
+                    <th>{{ __('monthlyPRpageLang.buyprice') . '(' . $Currency2 . ' ' . $Rate . ')' }}</th>
+                    <th>{{ __('monthlyPRpageLang.moq') }}</th>
                 </tr>
-            @endfor
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @for ($a = 0; $a < count($PN); $a++)
+                    <tr>
+                        <td>{{ $PN[$a] }}</td>
+                        <td>{{ $pName[$a] }}</td>
+                        <td>{{ $Spec[$a] }}</td>
+                        <td>{{ $Unit_price[$a] }}</td>
+                        <td>{{ $nowNeed[$a] }}</td>
+                        <td>{{ $nextNeed[$a] }}</td>
+                        <td>{{ $Stock[$a] }}</td>
+                        <td>{{ $in_Transit[$a] }}</td>
+                        <td>{{ $ReqAmount[$a] }}</td>
+                        <td>{{ $total_price1[$a] }}</td>
+                        <td>{{ $total_price2[$a] }}</td>
+                        <td>{{ $MOQ[$a] }}</td>
+                    </tr>
+                @endfor
+            </tbody>
+        </table>
 </body>
 
 </html>
