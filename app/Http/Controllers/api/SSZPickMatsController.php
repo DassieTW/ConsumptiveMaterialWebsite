f14a88e169744cecb0f9b5ed22e826c7d6f3742f
Pegatron <pega@Pegatrons-MacBook-Pro>
2024-03-12 14:46:57 +0800

    test

--- app/Http/Controllers/api/SSZPickMatsController.php
+++ app/Http/Controllers/api/SSZPickMatsController.php
@@ -27,7 +27,7 @@ class SSZPickMatsController extends Controller
     {
         \Config::set('database.connections.' . env("DB_CONNECTION") . '.database', "Consumables management");
         \DB::purge(env("DB_CONNECTION"));
-        $path = public_path('temp_test_mis.json');
+        $path = storage_path('temp_test_mis.json');
         file_put_contents($path, json_encode($request) . "//-----------------------//");
         return \Response::json(['message' => 'test successful']/* Status code here default is 200 ok*/);
     } // storeDataFromMIS
--- resources/views/member/change.blade.php
+++ resources/views/member/change.blade.php
@@ -57,7 +57,7 @@
                         </tr>
                         {{-- 基礎資料 --}}
                         <tr>
-                            <td rowspan="20">1</td>
+                            <td rowspan="42">1</td>
                             <td rowspan="3" class="table-success">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                     class="bi bi-info-circle align-middle me-2" viewBox="0 0 16 16">
@@ -168,28 +168,84 @@
                         <tr>
                             <td rowspan="8" class="table-success">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-inboxes align-middle me-2" viewBox="0 0 16 16">
+                                    class="bi bi-cart2 align-middle me-2" viewBox="0 0 16 16">
                                     <path
-                                        d="M4.98 1a.5.5 0 0 0-.39.188L1.54 5H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0A.5.5 0 0 1 10 5h4.46l-3.05-3.812A.5.5 0 0 0 11.02 1H4.98zm9.954 5H10.45a2.5 2.5 0 0 1-4.9 0H1.066l.32 2.562A.5.5 0 0 0 1.884 9h12.234a.5.5 0 0 0 .496-.438L14.933 6zM3.809.563A1.5 1.5 0 0 1 4.981 0h6.038a1.5 1.5 0 0 1 1.172.563l3.7 4.625a.5.5 0 0 1 .105.374l-.39 3.124A1.5 1.5 0 0 1 14.117 10H1.883A1.5 1.5 0 0 1 .394 8.686l-.39-3.124a.5.5 0 0 1 .106-.374L3.81.563zM.125 11.17A.5.5 0 0 1 .5 11H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0 .5.5 0 0 1 .5-.5h5.5a.5.5 0 0 1 .496.562l-.39 3.124A1.5 1.5 0 0 1 14.117 16H1.883a1.5 1.5 0 0 1-1.489-1.314l-.39-3.124a.5.5 0 0 1 .121-.393zm.941.83.32 2.562a.5.5 0 0 0 .497.438h12.234a.5.5 0 0 0 .496-.438l.32-2.562H10.45a2.5 2.5 0 0 1-4.9 0H1.066z" />
+                                        d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
                                 </svg>
-                                <span class="align-middle">{!! __('templateWords.outbound') !!}</span>
+                                <span class="align-middle">{!! __('templateWords.monthly') !!}</span>
                             </td>
-                            <td class="table-success"><span>{!! __('outboundpageLang.pick') !!}</span></td>
+                            <td class="table-success"><span>{!! __('templateWords.isnConsumeAdd') !!}</span></td>
                         </tr>
                         <tr>
-                            <td class="table-success"><span>{!! __('outboundpageLang.picklist') !!}</span></td>
+                            <td class="table-success"><span>{!! __('templateWords.isnConsumeUpdate') !!}</span></td>
                         </tr>
                         <tr>
-                            <td class="table-success"><span>{!! __('outboundpageLang.pickrecord') !!}</span></td>
+                            <td class="table-success"><span>{!! __('templateWords.importMonthlyData') !!}</span></td>
                         </tr>
                         <tr>
-                            <td class="table-success"><span>{!! __('outboundpageLang.back') !!}</span></td>
+                            <td class="table-success"><span>{!! __('templateWords.importNonMonthlyData') !!}</span></td>
                         </tr>
                         <tr>
-                            <td class="table-success"><span>{!! __('outboundpageLang.backlist') !!}</span></td>
+                            <td class="table-success"><span>{!! __('monthlyPRpageLang.UpdateUnitPrice') !!}</span></td>
                         </tr>
                         <tr>
-                            <td class="table-success"><span>{!! __('outboundpageLang.backrecord') !!}</span></td>
+                            <td class="table-success"><span>{!! __('monthlyPRpageLang.PR') !!}</span></td>
+                        </tr>
+                        <tr>
+                            <td class="table-success"><span>{!! __('monthlyPRpageLang.SXB_search') !!}</span></td>
+                        </tr>
+                        <tr>
+                            <td class="table-success"><span>{!! __('templateWords.on_the_way_search') !!}</span></td>
+                        </tr>
+                        {{-- ----------------------------------------------------------------------------------------- --}}
+                        <tr class="table-secondary">
+                            <td colspan="2"></td>
+                        </tr>
+                        {{-- ----------------------------------------------------------------------------------------- --}}
+                        {{-- O庫 --}}
+                        <tr>
+                            <td rowspan="12" class="table-success">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-box-seam align-middle me-2" viewBox="0 0 16 16">
+                                    <path
+                                        d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2l-2.218-.887zm3.564 1.426L5.596 5 8 5.961 14.154 3.5l-2.404-.961zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z" />
+                                </svg>
+                                <span class="align-middle">{!! __('templateWords.obound') !!}</span>
+                            </td>
+                            <td class="table-success"><span>{!! __('oboundpageLang.newMats') !!}</span></td>
+                        </tr>
+                        <tr>
+                            <td class="table-success"><span>{!! __('oboundpageLang.matsInfo') !!}</span></td>
+                        </tr>
+                        <tr>
+                            <td class="table-success"><span>{!! __('oboundpageLang.inbound') !!}</span></td>
+                        </tr>
+                        <tr>
+                            <td class="table-success"><span>{!! __('oboundpageLang.inboundsearch') !!}</span></td>
+                        </tr>
+                        <tr>
+                            <td class="table-success"><span>{!! __('oboundpageLang.stockupload') !!}</span></td>
+                        </tr>
+                        <tr>
+                            <td class="table-success"><span>{!! __('oboundpageLang.searchstock') !!}</span></td>
+                        </tr>
+                        <tr>
+                            <td class="table-success"><span>{!! __('oboundpageLang.pick') !!}</span></td>
+                        </tr>
+                        <tr>
+                            <td class="table-success"><span>{!! __('oboundpageLang.picklist') !!}</span></td>
+                        </tr>
+                        <tr>
+                            <td class="table-success"><span>{!! __('oboundpageLang.pickrecord') !!}</span></td>
+                        </tr>
+                        <tr>
+                            <td class="table-success"><span>{!! __('oboundpageLang.back') !!}</span></td>
+                        </tr>
+                        <tr>
+                            <td class="table-success"><span>{!! __('oboundpageLang.backlist') !!}</span></td>
+                        </tr>
+                        <tr>
+                            <td class="table-success"><span>{!! __('oboundpageLang.backrecord') !!}</span></td>
                         </tr>
 
                         <tr class="table-secondary">
